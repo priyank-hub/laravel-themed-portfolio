03a08bb3280b90c2a9b97c321cefb228c498dd3e
Priyank <patelpriyank177@gmail.com>
2023-03-09 14:02:24 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
     <div class="" style="position: fixed; top: 80px; right: 20px">
-      <span class="cursor-pointer">
+      <!-- <span class="cursor-pointer">
         <i v-if="mode == 'dark'" class="fas fa-sun text-white" @click="switchMode('light')"></i>
         <i v-else-if="mode == 'light'" class="fas fa-moon" @click="switchMode('dark')"></i>
-      </span>
+      </span> -->
+
+      <label for="night-light-checkbox" class="night-light-label" :class="mode == 'dark' ? 'bg-black' : 'bg-white'">
+        <input type="checkbox" id="night-light-checkbox">
+        <span class="night-light-ball"></span>
+        <svg viewBox="0 0 512 512" class="sun-svg" :class="mode == 'dark' ? 'fill-white' : 'fill-black'">
+          <path id="sun-svg" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z">
+          </path>
+        </svg>
+        <svg viewBox="0 0 512 512" class="moon-svg" :class="mode == 'dark' ? 'fill-white' : 'fill-black'">
+          <path id="moon-svg" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z">
+          </path>
+        </svg>
+      </label>
     </div>
 
     <div class="nav-bar" :class="mode == 'dark' ? 'bg-dark-grey' : 'bg-light-grey'">
@@ -209,6 +222,31 @@ export default {
   computed: {
 
   },
+  
+  mounted() {
+    const darkCheck = document.getElementById('night-light-checkbox');
+
+    darkCheck.addEventListener('click', () => {
+      if (darkCheck.checked) {
+        this.mode = 'dark';
+        document.body.style.backgroundColor = '#171923'
+        // document.body.classList.add('dark');
+        // localStorage.setItem('animate-css', 'dark');
+      } 
+      else {
+        this.mode = 'light';
+        document.body.style.backgroundColor = 'transparent'
+        // document.body.classList.remove('dark');
+        // localStorage.removeItem('animate-css');
+      }
+    })
+
+    // if (localStorage.getItem('animate-css')) {
+    //   document.body.className = 'dark';
+    //   darkCheck.checked = true;
+    // }
+  },
+
   methods: {
     switchMode(mode) {
       this.mode = mode;
@@ -232,6 +270,62 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.fill-white {
+  fill: white;
+}
+
+.fill-black {
+  fill: black;
+}
+
+.night-light-label {
+  display: block;
+  margin: 20px 0;
+  height: 31px;
+  width: 60px;
+  border: 2px solid #FF2C1F;
+  border-radius: 30px;
+  position: relative;
+  cursor: pointer;
+}
+
+.night-light-label #night-light-checkbox {
+  position: absolute;
+  visibility: hidden;
+}
+
+.night-light-label .night-light-ball {
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  top: 1px;
+  left: 1px;
+  border-radius: 50%;
+  background: #FF2C1F;
+  z-index: 99;
+  transition: 300ms;
+}
+
+.moon-svg, .sun-svg {
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.sun-svg {
+  left: 5px;
+}
+
+.moon-svg {
+  right: 5px;
+}
+
+.night-light-label #night-light-checkbox:checked + .night-light-ball {
+  transform: translateX(28px);
 }
 
 .nav-links li {
